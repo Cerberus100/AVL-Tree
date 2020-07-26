ddbb06e489f288dc6b893ae9f4815b9902462cc6
Luke Collins <luke23571113@gmail.com>
2020-07-26 12:27:52 -0700

    Add project files.

---
+++ AVL Tree/AVL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVL_Tree
+{
+    class AVL
+    {
+    }
+}
---
+++ AVL Tree/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVL_Tree
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        T value;
+        public Node<T> parent { get; set; }
+        public Node<T> leftChild { get; set; }
+        public Node<T> rightChild { get; set; }
+
+        int childCount 
+        {
+            get
+            {
+                int count = 0;
+
+                if (leftChild != null) count++;
+                if (rightChild != null) count++;
+
+                return count; 
+            }
+        }
+
+        int height { get; set; }
+
+        int balance
+        {
+            get
+            {
+
+            }
+        }
+
+        public Node(T Value)
+        {
+            value = Value;
+            height = 1; 
+        }
+
+    }
+}
