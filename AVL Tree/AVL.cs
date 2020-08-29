e28f39b8b6e1c185b1c1eecf2538520e4f7af48e
Luke Collins <Luke.Collins@GMR.local>
2020-08-29 16:03:34 -0700

    Finished node class, worked on rotations

--- AVL Tree/AVL.cs
+++ AVL Tree/AVL.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AVL_Tree
 {
-    class AVL
+    public class AVL<T> where T : IComparable<T>
     {
+        Node<T> root = null;
+
+        public void Balance(Node<T> node)
+        {
+            if (node.balance >= -1 && node.balance <= 1) return;
+
+
+        }
+
+        public void rightRotate(Node<T> node)
+        {
+            //Check if root or if it has a parent.  change the root.  Change the parents connection
+            Node<T> temp = node.leftChild.rightChild;
+
+            node.leftChild.rightChild = node;
+            node.leftChild.parent = node.parent;
+
+            if(node == root)
+            {
+
+            }
+            node.parent = node.leftChild;
+
+            node.leftChild = temp;    
+
+            //TODO: Check to see if the node has is root or has parent nodes. If it is root. then you don't have to reconnect the left child. 
+            //Else, find out if node is left or right child and connect it. 
+        }
+
+
     }
 }
--- AVL Tree/Node.cs
+++ AVL Tree/Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AVL_Tree
@@ -10,6 +11,24 @@ namespace AVL_Tree
         public Node<T> parent { get; set; }
         public Node<T> leftChild { get; set; }
         public Node<T> rightChild { get; set; }
+
+        public bool isLeft
+        {
+            get
+            {
+                if (parent == null || parent.leftChild != this) return false;
+                return true;
+            }
+        }
+
+        public bool isRight
+        {
+            get
+            {
+                if (parent == null || parent.rightChild != this) return false;
+                return true; 
+            }
+        }
 
         public int childCount
         {
@@ -24,21 +43,58 @@ namespace AVL_Tree
             }
         }
 
-        public int height { get; set; }
+        public int height
+        {
+            get
+            {
+                int leftHeight = 0;
+
+                if(leftChild != null)
+                {
+                    leftHeight = leftChild.height; 
+
+                }
+
+
+                int rightHeight = 0; 
+
+                if(rightChild != null)
+                {
+                    rightHeight = rightChild.height; 
+                }
+
+                if (leftHeight <= rightHeight) return rightHeight + 1;
+                else return leftHeight + 1; 
+            }
+      
+        }
 
         public int balance
         {
             get
             {
-
+                if (leftChild == null && rightChild == null)
+                {
+                    return 0;
+                }
+                else if (leftChild == null && rightChild != null)
+                {
+                    return rightChild.height;
+                }
+                else if (leftChild != null && rightChild == null)
+                {
+                    return 0 - leftChild.height;
+                }
+                else
+                {
+                    return rightChild.height - leftChild.height;
+                }
             }
         }
 
         public Node(T Value)
         {
             value = Value;
-            height = 1;
         }
-
     }
 }
